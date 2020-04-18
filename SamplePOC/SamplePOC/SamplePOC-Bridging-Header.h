d3ca1946bbdca62a7a0ff0183697ac96ef202ba3
Rajesh Yadav <rajesh.yadav@consagous.com>
2020-04-18 10:04:26 +0530

    Add Bridging header file for using thrid party libaries in code

---
+++ SamplePOC/SamplePOC/SamplePOC-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  SamplePOC-Bridging-Header.h
+//  SamplePOC
+//
+//  Created by Rajesh Yadav on 18/04/20.
+//  Copyright © 2020 Rajesh Yadav. All rights reserved.
+//
+
+#ifndef SamplePOC_Bridging_Header_h
+#define SamplePOC_Bridging_Header_h
+
+
+#endif /* SamplePOC_Bridging_Header_h */
+
+@import SDWebImage;
+@import Masonry;
